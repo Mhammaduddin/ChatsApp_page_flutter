c86fa064dd62c15a103a18bd38c097f027f46e6e
Mhammaduddin <64686639+Mhammaduddin@users.noreply.github.com>
2021-10-16 20:41:11 +0500

    ChatsApp page

---
+++ flutter_whatsapp_page.dart
@@ -0,0 +1,86 @@
+[6:06 PM, 10/16/2021] Muhammad Hammad Uddin: import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,	
+      home: Scaffold(
+	appbar: AppBar(
+	litle: Text('WhatsApp'),
+	actions:[Icon(Icons.search)],
+        shodowColor: Colors.green[300],
+	bottom: new TabBar(
+        controller: controller,
+	color: Colors.green[300],
+        tabs: <Tab>[
+          new Tab('Chats',icon: new Icon(Icons.arrow_forward)),
+          new Tab('Status', icon: new Icon(Icons.arrow_downward)),
+          new Tab('Calls',icon: new Icon(Icons.arrow_back)),
+        ]
+      )
+),
+),
+	body: Container(
+	child: …
+[6:06 PM, 10/16/2021] Muhammad Hammad Uddin: import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,	
+      home: Scaffold(
+	appbar: AppBar(
+	litle: Text('WhatsApp'),
+	actions:[Icon(Icons.search)],
+        shodowColor: Colors.green[300],
+	bottom: new TabBar(
+        controller: controller,
+	color: Colors.green[300],
+        tabs: <Tab>[
+          new Tab('Chats',icon: new Icon(Icons.arrow_forward)),
+          new Tab('Status', icon: new Icon(Icons.arrow_downward)),
+          new Tab('Calls',icon: new Icon(Icons.arrow_back)),
+        ]
+      )
+),
+),
+	body: Container(
+	child: Column(
+	children:[
+	chats("hammad","kaha ho bhai"),
+	chats("ammar","oye"),
+	chats("ali","hello"),
+],
+),
+),
+	
+	
+),
+    );
+  }
+}
+
+Widget chats( string name, string text){
+return ListTile(
+tileColor: Colors.amber[50],
+leading: CircleAvatar(
+backgroundColor: Colors.blueGrey,radius:25,
+),
+title: Text(name),
+subtitle: Text(text),
+trailing: Text("9:55"),
+)}
